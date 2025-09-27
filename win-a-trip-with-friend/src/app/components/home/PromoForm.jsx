c663282f1e734def8e40c8a85e36ad0acb6520e7
richurichu <paulsonrichu@gmail.com>
2025-09-27 15:10:57 +0530

    Home page completed

---
+++ win-a-trip-with-friend/src/app/components/home/PromoForm.jsx
@@ -0,0 +1,135 @@
+"use client";
+
+import { useState } from "react";
+
+export default function PromoForm() {
+
+    const formFieldsStyle = 'w-full p-1.5  bg-white outline-none  placeholder:font-bold placeholder:text-red-600'
+
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        mobileNumber: "",
+        uniqueCode: "",
+        agree: false,
+    });
+
+    const handleChange = (e) => {
+        const { name, value, type, checked } = e.target;
+        setFormData((prev) => ({
+            ...prev,
+            [name]: type === "checkbox" ? checked : value,
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!formData.agree) {
+            alert('please agree it ')
+        }
+        console.log("Form Submitted:", formData);
+        // You can send `formData` to your backend API here
+    };
+
+    return (
+        <form
+            onSubmit={handleSubmit}
+            className="w-full flex flex-col  items-center  mx-auto p-6 space-y-6 "
+        >
+            <input
+                type="text"
+                name="firstName"
+                placeholder="First Name"
+                value={formData.firstName}
+                onChange={handleChange}
+                required
+                className={formFieldsStyle}
+            />
+
+            <input
+                type="text"
+                name="lastName"
+                placeholder="Last Name"
+                value={formData.lastName}
+                onChange={handleChange}
+                required
+                className={formFieldsStyle}
+            />
+
+            <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+                className={formFieldsStyle}
+            />
+
+            <input
+                type="tel"
+                name="mobileNumber"
+                placeholder="Mobile Number"
+                value={formData.mobileNumber}
+                onChange={handleChange}
+                required
+                className={formFieldsStyle}
+            />
+
+            <input
+                type="text"
+                name="uniqueCode"
+                placeholder="Enter Unique Code Here"
+                value={formData.uniqueCode}
+                onChange={handleChange}
+                required
+                className={formFieldsStyle}
+            />
+
+            <div className="flex xl:mt-2 2xl:mt-4 space-x-2 md:w-10/12 mx-auto">
+                <label className="flex items-center cursor-pointer">
+                    {/* Hidden native checkbox to handle form state */}
+                    <input
+                        type="checkbox"
+                        name="agree"
+                        checked={formData.agree}
+                        onChange={handleChange}
+                        className="sr-only" // hides the native checkbox
+                    />
+                    <div
+                        className={` size-3 lg:size-4 border  border-white flex items-center justify-center
+                  bg-transparent`}
+                    >
+                        {formData.agree && (
+                            <svg
+                                className="w-3 h-3 text-white"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                viewBox="0 0 24 24"
+                            >
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
+                            </svg>
+                        )}
+                    </div>
+                </label>
+                <p className="text-xs text-center text-white">
+                    Yes, I agree to receive news, offers and promotions via email from
+                    The Star Gold Coast relating to the hotel, theatre, restaurants and
+                    events. You can opt out directly with The Star at anytime. Please
+                    share my personal information with The Star Gold Coast for this
+                    purpose.
+                </p>
+            </div>
+
+
+            <button
+                type="submit"
+                className="w-56 bg-black text-white py-1.5 rounded-full text-lg font-bold hover:bg-gray-800 transition"
+            >
+                Submit
+            </button>
+        </form>
+    );
+}
---
+++ win-a-trip-with-friend/src/app/page.jsx
@@ -0,0 +1,83 @@
+import PromoForm from "./components/home/PromoForm";
+
+export default function Home() {
+  return (
+    // <div className="h-[150dvh]  relative bg-black">
+    //   <div className="absolute inset-0 left-1/2 -translate-x-1/2 w-9/12 h-8/12">
+    //     <img
+    //       src="/image/home/banner.jpg"
+    //       alt="Banner"
+    //       className="w-full h-full"
+    //     />
+    //   </div>
+
+    //   <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-9/12 h-7/12">
+    //     <img
+    //       src="/image/home/pattern.png"
+    //       alt="Pattern"
+    //       className="w-full h-full"
+    //     />
+    //   </div>
+    // </div>
+
+    <div className="w-full h-fit flex justify-center bg-black items-center">
+      <div className="lg:w-9/12 w-full min-h-[130dvh]  relative">
+        <div className="absolute inset-0 w-full h-7/12">
+          <img
+            src="/image/home/banner.jpg"
+            alt="Banner"
+            className="w-full h-full"
+          />
+        </div>
+        <div className="absolute bottom-0 w-full h-7/12">
+          <img
+            src="/image/home/pattern.png"
+            alt="Pattern"
+            className="w-full h-full"
+          />
+        </div>
+
+        <div className=" w-full h-full  absolute inset-0">
+          <div className=" pt-10 2xl:pt-16 lg:w-6/12 sm:w-9/12 w-11/12  h-full mx-auto flex  flex-col items-center">
+
+
+            <div className=" 2xl:w-7/12 w-7/12 space-x-6 h-fit  flex items-center justify-between  ">
+              {/* coke logo */}
+              <div className="w-fit h-20 2xl:h-24 ">
+                <img src="/image/home/coke-logo.png" alt="coke-logo" className=" w-full h-full object-contain" />
+              </div>
+              {/* star logo */}
+              <div className="w-fit  h-12 2xl:h-16 ">
+                <img src="/image/home/star-logo.png" alt="star-logo" className=" w-full h-full object-contain" />
+              </div>
+            </div>
+
+            {/* main text */}
+            <div className="w-full  h-fit 2xl:mt-16 md:mt-10 mt-5 ">
+              <img src="/image/home/info-text.png" alt="info" className=" w-full h-full object-contain" />
+            </div>
+
+            {/* form */}
+            <div className="2xl:mt-16 xl:mt-12 mt-6  relative">
+              <PromoForm />
+
+            </div>
+            {/* <div className="w-full bor absolute -bottom-56 left-72  size-96  "> */}
+            <div className="w-fit  absolute  right-1  2xl:-right-12  xl:bottom-10 lg:bottom-36 bottom-0 size-[16%]  sm:size-[28%] lg:size-[32%] xl:size-[35%]   2xl:size-[37%]  ">
+              <img src="/image/home/cola.png" alt="coke" className=" w-full h-full object-contain" />
+            </div>
+
+            <div className="2xl:w-[350px] lg:w-[300px] max-sm:w-[200px] h-fit  absolute bottom-1 sm:bottom-8 lg:bottom-28 xl:bottom-20 2xl:bottom-20 sm:left-5 left-2 lg:left-0 xl:left-5  ">
+              <p className=" max-w-xs text-center mb-4 max-sm:text-sm text-white md:font-semibold">Purchase any of these Coca-Cola products to participate  </p>
+              <img src="/image/home/brand logos.png" alt="brands" className=" w-full h-full object-contain" />
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+
+  );
+}
