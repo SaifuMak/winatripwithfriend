a43cd8f057a9e39dc9ad50842bfb0e4664ed302f
richurichu <paulsonrichu@gmail.com>
2025-09-30 14:37:09 +0530

    ssl

--- win-a-trip-with-friend/src/app/lib/axios.js
+++ win-a-trip-with-friend/src/app/lib/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const AXIOS_INSTANCE = axios.create({
     // baseURL: process.env.NEXT_PUBLIC_API_BASE || 'http://localhost:8000/',
-    baseURL: process.env.NEXT_PUBLIC_API_BASE || 'http://app.winatripwithafriend.com/',
+    baseURL: process.env.NEXT_PUBLIC_API_BASE || 'https://app.winatripwithafriend.com/',
 
     withCredentials: true, // if using cookies for auth
 });
